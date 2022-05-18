7dde08cf6857319fc7b2d4747a59ddb8b3b31ebc
ido90 <idogreenberg90@gmail.com>
2022-05-18 13:53:18 +0300

    update package name

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="CEM",
+    name="cross-entropy-method",
     version="0.0.1",
     license='MIT',
     author="Ido Greenberg",
